38bdc0520d2e7b4345fcacf2a7617e7abd2a11a0
Arlth <windowz@live.ru>
2021-11-17 16:51:53 +0200

    Финальный фикс якорей

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,11 @@
     const mobileMenu = document.querySelector('.js-menu-container');
     const openMenuBtn = document.querySelector('.js-open-menu');
     const closeMenuBtn = document.querySelector('.js-close-menu');
+    const mobileMenuHome = document.querySelector('#home-link');
+    const mobileMenuAbout = document.querySelector('#about-link');
+    const mobileMenuCustomers = document.querySelector('#customer-link');
+    const mobileMenuProducts = document.querySelector('#products-link');
+    const mobileMenuContacts = document.querySelector('#contacts-link');
   
     const toggleMenu = () => {
       const isMenuOpen =
@@ -12,6 +17,12 @@
   
     openMenuBtn.addEventListener('click', toggleMenu);
     closeMenuBtn.addEventListener('click', toggleMenu);
+    mobileMenuHome.addEventListener('click', toggleMenu);
+    mobileMenuCustomers.addEventListener('click', toggleMenu);
+    mobileMenuAbout.addEventListener('click', toggleMenu);
+    mobileMenuProducts.addEventListener('click', toggleMenu);
+    mobileMenuContacts.addEventListener('click', toggleMenu);
+
   
     // Закрываем мобильное меню на более широких экранах
     // в случае изменения ориентации устройства.
@@ -21,22 +32,4 @@
       openMenuBtn.setAttribute('aria-expanded', false);
       bodyScrollLock.enableBodyScroll(docmobile__navigate-linkument.body);
     });
-
-    // Закрываем мобильное меню после выбора секции 
-    $(".mobile__navigate-link").click(function () {
-      if ($(".menu__container").hasClass('is-open')) {
-        $(".menu__container").removeClass('is-open');
-      }
-    });
-    //Плавный переход на верх при нажатии Home *может быть задействован так же для будущей кнпки UP
-    (function($) {
-      $(function() {
-      
-        $('#home-link').click(function() {
-          $('html, body').animate({scrollTop: 0},500);
-          return false;
-        })
-      
-      })
-      })(jQuery)
   })();
